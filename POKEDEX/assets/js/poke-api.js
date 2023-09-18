60c6ab87c7be7ee25f8d162e36e925f009de3a59
Henrique Brenner <henriquebrenneralvesmatias@gmail.com>
2023-09-18 17:46:41 -0300

    refatorando codigo e criando chamado para API

--- POKEDEX/assets/js/poke-api.js
+++ POKEDEX/assets/js/poke-api.js
@@ -7,5 +7,18 @@ pokeApi.getPokemons = (offset = 0, limit = 10) => {
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
-        .catch((error) => console.log(error))
+        .then((pokemons) => pokemons.map((pokemon) => fetch(pokemon.url)))
 }
+
+
+Promise.all([
+    fetch('https://pokeapi.co/api/v2/pokemon/1/'),
+    fetch('https://pokeapi.co/api/v2/pokemon/2/'),
+    fetch('https://pokeapi.co/api/v2/pokemon/3/'),
+    fetch('https://pokeapi.co/api/v2/pokemon/4/'),
+    fetch('https://pokeapi.co/api/v2/pokemon/5/'),
+    fetch('https://pokeapi.co/api/v2/pokemon/6/'),
+    fetch('https://pokeapi.co/api/v2/pokemon/7/')
+]).then(results => {
+    console.log(results);
+})
