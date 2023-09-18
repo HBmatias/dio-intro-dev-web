95ef8df9226b0dd4a58a8430a4e090261598b98c
Henrique Brenner <henriquebrenneralvesmatias@gmail.com>
2023-09-18 17:59:57 -0300

    criando buscador de detalhe

--- POKEDEX/assets/js/poke-api.js
+++ POKEDEX/assets/js/poke-api.js
@@ -2,23 +2,21 @@
 
 const pokeApi = {}
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+    .then((response) => response.json())
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
-        .then((pokemons) => pokemons.map((pokemon) => fetch(pokemon.url)))
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => {
+            debugger
+            console.log(pokemonsDetails)
+        })
 }
 
-
-Promise.all([
-    fetch('https://pokeapi.co/api/v2/pokemon/1/'),
-    fetch('https://pokeapi.co/api/v2/pokemon/2/'),
-    fetch('https://pokeapi.co/api/v2/pokemon/3/'),
-    fetch('https://pokeapi.co/api/v2/pokemon/4/'),
-    fetch('https://pokeapi.co/api/v2/pokemon/5/'),
-    fetch('https://pokeapi.co/api/v2/pokemon/6/'),
-    fetch('https://pokeapi.co/api/v2/pokemon/7/')
-]).then(results => {
-    console.log(results);
-})
