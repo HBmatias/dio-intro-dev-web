833d4af997d36bacd75a2b12b09d4dfa2e9e2db4
Henrique Brenner <henriquebrenneralvesmatias@gmail.com>
2023-09-19 10:15:57 -0300

    criando modelo pokemon

--- POKEDEX/assets/js/main.js
+++ POKEDEX/assets/js/main.js
@@ -3,20 +3,18 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-function convertPokemonTypesLi(pokemonTypes) {
-    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
-}
+
 function convertPokemonToLi(pokemon) {
     return `
     <li class="pokemon">
-        <span class="number">#${pokemon.order}</span>
+        <span class="number">#${pokemon.number}</span>
         <span class="name">${pokemon.name}</span>
 
         <div class="detail">
             <ol class="types">
-                ${convertPokemonTypesLi(pokemon.types).join('')}
+                ${pokemon.types.map((type) => `<li class="type">${type}</li>`)}
             </ol>
-            <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
+            <img src="${pokemon.photo}" alt="${pokemon.name}">
         </div>
     </li>
     `
