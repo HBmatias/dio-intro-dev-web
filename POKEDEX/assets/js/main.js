ead999375667078ad55980576afc58b173f8bf51
Henrique Brenner <henriquebrenneralvesmatias@gmail.com>
2023-09-20 17:53:06 -0300

    termino da paginação e limite de geração.

--- POKEDEX/assets/js/main.js
+++ POKEDEX/assets/js/main.js
@@ -2,8 +2,8 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
 
-const maxRecords = 11
-const limit = 5
+const maxRecords = 151
+const limit = 10
 let offset = 0;
 
 function loadPokemonItens(offset, limit) {
@@ -32,5 +32,14 @@ loadPokemonItens(offset, limit)
 
 loadMoreButton.addEventListener('click', () => {
     offset += limit
-    loadPokemonItens(offset, limit)
+    const qtdRecordWithNexPage = offset + limit
+
+    if (qtdRecordWithNexPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    }else{
+        loadPokemonItens(offset, limit)
+    }
 })
